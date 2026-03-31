7830cba1eebf8e3cfad09fd6b58e9da94b42a028
manuraj23 <manuraj082004@gmail.com>
2026-03-31 15:55:56 +0530

    Updated SpringSecurity

--- src/main/java/com/FormFlow/FormFlow/Config/SpringSecurity.java
+++ src/main/java/com/FormFlow/FormFlow/Config/SpringSecurity.java
@@ -47,7 +47,7 @@ public class SpringSecurity {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("http://localhost:4200"));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT","PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
 
