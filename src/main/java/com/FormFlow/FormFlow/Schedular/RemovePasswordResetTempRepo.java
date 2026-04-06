bb671c84b8a8fc20cd654ae71baec016f5d3f51d
manuraj23 <manuraj082004@gmail.com>
2026-04-06 20:32:39 +0530

    Reduced Timing of Schedular

--- src/main/java/com/FormFlow/FormFlow/Schedular/RemovePasswordResetTempRepo.java
+++ src/main/java/com/FormFlow/FormFlow/Schedular/RemovePasswordResetTempRepo.java
@@ -11,7 +11,7 @@ public class RemovePasswordResetTempRepo {
     @Autowired
     private PasswordResetTempRepository passwordResetTempRepository;
 
-    @Scheduled(cron = "0 */10 * * * *")
+    @Scheduled(cron = "0 */5 * * * *")
     public void removePasswordResetTempRepo() {
         passwordResetTempRepository.deleteByOtpExpiry(LocalDateTime.now());
         System.out.println("Expired password reset tokens removed at: " + LocalDateTime.now());
